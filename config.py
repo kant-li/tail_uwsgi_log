73aa9f9ce88259641fb051fc882589a4b38ca2fc
kant-li <lirongxin14@qq.com>
2019-10-09 11:03:49 +0800

    Add basic email func

--- config.py
+++ config.py
@@ -1,8 +1,18 @@
 #! /usr/bin/python
 # -*- coding:utf8 -*-
 
+from credentials import mail_addr, password
+
+
+class Emailconfig:
+    """邮件发送配置类"""
+    mail_host = 'smpt.qq.com'
+    mail_addr = mail_addr
+    password = password
+
 
 class Logconfig:
+    """日志文件解析配置类，如文件地址、解析方式、错误通知人等"""
     pattern = r'''\]\ (?P<ip>.*?)\ (.*)\ {.*?}\ \[(?P<datetime>.*?)\]\ (?P<request_method>POST|GET|DELETE|PUT|PATCH)\s
             (?P<request_uri>[^ ]*?)\ =>\ generated\ (?:.*?)\ in\ (?P<resp_msecs>\d+)\ msecs\s
             \(HTTP/[\d.]+\ (?P<resp_status>\d+)\)'''
---
+++ credentials.py
@@ -0,0 +1,5 @@
+#! /usr/bin/python
+# -*- coding: utf8 -*-
+
+mail_addr = ''
+password = ''
